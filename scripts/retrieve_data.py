043f1ad128e8f2fd9a687f7ef904f448aa4a984e
JeffPersons <jperman8@gmail.com>
2016-11-11 08:28:30 -0800

    Enhanced data retrieval script
    Added the following features to the script retrieve_data.py:
    - If an error occurs during download of an email's attachments, remove
    all the downloaded files for that email
    - Sensitive info (passwords, etc) are now read from outside the source
    via python environment variables (from a config file locally, from
    heroku config settings on the server)
    - Only the most recent unread email is read and extracted from (in
    contrast to ALL in the previous proof-of-concept script)
    - The email is now checked for the apropriate sender and subject title
    (for obvious security reasons)

--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -1,38 +1,43 @@
 """Work in progress script for extracting auto generated emails from the stem center data system."""
-import email
-import imaplib
-from typing import List
+import os
 
-import dev_settings
-from stem_center_analytics.utils import os_lib, io_lib
+import dev_settings  # import the set env variables
 from stem_center_analytics import SCRIPT_DIR
+from stem_center_analytics.utils import os_lib, io_lib
+
+
+ATTACHMENTS = ('unclean_tutor_requests', 'unclean_student_logins')  # for use later on
 
 
 def main():
     # Starred messages mean that they were marked as read/attempted to extract/save,
-    # but it failed, so their starred to indicate error occurred, and to manually deal with later
-    with io_lib.connect_to_imap_server(*dev_settings.IMAP_SERVER_INFO._asdict().values()) as con:
-        email_ids = io_lib.get_unread_email_uids(con)
-        for email_id in email_ids:
-            try:
-                io_lib.download_email_attachment(con, email_id, output_dir=SCRIPT_DIR)
-                raise ValueError  # if uncommented, will flag error
-            except Exception as e:
-                print(e)
-                print('attachment download failed')
-                con.uid('STORE', email_id, '+FLAGS', '\\Flagged')
+    # but it failed, so their starred to indicate error occurred, and to manually recover later
+
+    with io_lib.connect_to_imap_server(
+        server_host='imap.gmail.com',
+        user_name=os.environ.get('EMAIL_SERVER_USERNAME'),
+        user_password=os.environ.get('EMAIL_SERVER_PASSWORD')
+    ) as con:
+        file_names = []
+        # get newest unread email, if none then end the script
+        email_uids = io_lib.get_unread_email_uids(con, sender=os.environ.get('EMAIL_SENDER'),
+                                                  subject='StemCenterData')
+        if not email_uids:
+            exit(0)
+        try:
+            file_names = io_lib.download_all_email_attachments(con, email_uids[0], SCRIPT_DIR)
+        except Exception as e:
+            print(e)  # print traceback
+            print('attachment download failed')
+            for file_name in file_names:
+                os_lib.remove_file(file_name, ignore_errors=True)
+            con.uid('STORE', email_uids[0], '+FLAGS', '\\Flagged')  # star the email
 
 
 if __name__ == '__main__':
     main()
 
 
-# fixme: only mark the email message as read if it was successfully extracted
 # todo: figure out how to deal with multiple file attachments, multiple unread, etc...
 # todo: figure out how to deal with persisting the temporary attachment through the various scripts
-
-# todo: incorporate the EXPECT_MESSAGE_INFO from dev_settings, and only read one at a time
-# mail_client.fetch(email_id, message_set)[0][1] ((BODY.PEEK[]) vs (RFC822))
-
-# the reason we mark are all as read, as otherwise, since the scheduler will run every hour,
-# and error ridden ones would be attempted to download again, etc...
+# todo: figure out details of logging traceback in the case of failed download
