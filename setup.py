61ca0c85b576e43386374d61c7af50bd46af29de
JeffPersons <jperman8@gmail.com>
2017-01-22 16:31:26 -0800

    Updated setup.py to reflect latest installation requirements

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
     packages=find_packages(),  # - or find_packages(where='stem_center_analytics')
     scripts=['\scripts'],
     install_requires=["python>=3.5.2",
-                      "cython>=0.24.1",
-                      "numpy>=1.11.1"
-                      "flask>=0.11.1",
-                      "pandas>=0.18.1"],
+                      "cython>=0.25.2",
+                      "numpy>=1.11.3"
+                      "flask>=0.12.0",
+                      "pandas>=0.19.2"],
     package_data={
         'docs': ['*.txt', '*.pdf', '*.rst'],       # documentation in text/restructured text/pdf (s)
         'external_datasets': ['*.json', '*.sql'],
@@ -27,11 +27,12 @@ setup(
     # Application author details:
     # https://github.com/FoothillCollege/StemAnalytics
     description="This is the backend for Foothill STEM Center's STEM Analytics Project.",
-    license="Open Source",
-    keywords="???",
+    license="TBD",
+    keywords="TBD",
     url="http://foothillcollege.github.io/StemAnalytics/",
     long_description="TBD",
     download_url="TBD",
 )
 
 # todo: verify setup/install works correctly in multiple envs (+ test scripts & packages)
+# attain MIT Open Source License, or similar...
